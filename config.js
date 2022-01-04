fa55add51973140c392cd6e6e45143fdaa79d1d2
therblxgod <64037997+therblxgod@users.noreply.github.com>
2022-01-03 20:47:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    "token": "OTI3NzAyMjkzODc5NjE5NjI0.YdOEFg.0FzcMv5DMsZtL_U0o1mTUnrR3jc",
+    "token": "",
     "prefix": "~",
     "MEMER_API_TOKEN": "", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
     "TENOR_API_KEY": "", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
     "mongooseConnectionString": "", 
-    "id": "927702293879619624",  // https://discordapp.com/developers/applications/ID/information,
-    "clientSecret": "zW7zN2uKxYfGpplciIvCp4bRWA9eyiAc",  // https://discordapp.com/developers/applications/ID/information,
+    "id": "",  // https://discordapp.com/developers/applications/ID/information,
+    "clientSecret": "",  // https://discordapp.com/developers/applications/ID/information,
     "domain": "http://localhost:3000",
     "port": 3000,
     "usingCustomDomain": false
